eb244e7bcc1c5d376c40459ff9ec2b6828ed9399
shreen-youssif <shreenyoussif22@gmail.com>
2024-01-21 03:31:48 -0800

    1-insertion_sort_list.c

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -9,31 +9,30 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *current, *temp;
+	listint_t *current, *temp;
 
-    if (!list || !(*list) || !(*list)->next)
-        return;
-
-    current = (*list)->next;
-
-    while (current)
-    {
-        temp = current;
-        while (temp->prev && temp->n < temp->prev->n)
-        {
-            /* Swap nodes and print the list */
-            if (temp->next)
-                temp->next->prev = temp->prev;
-            temp->prev->next = temp->next;
-            temp->next = temp->prev;
-            temp->prev = temp->prev->prev;
-            temp->next->prev = temp;
-            if (temp->prev)
-                temp->prev->next = temp;
-            else
-                *list = temp;
-            print_list(*list);
-        }
-        current = current->next;
-    }
+	if (!list || !(*list) || !(*list)->next)
+		return;
+	
+	current = (*list)->next;
+	while (current)
+	{
+		temp = current;
+		while (temp->prev && temp->n < temp->prev->n)
+		   {
+			   /* Swap nodes and print the list */
+			   if (temp->next)
+				   temp->next->prev = temp->prev;
+			   temp->prev->next = temp->next;
+			   temp->next = temp->prev;
+			   temp->prev = temp->prev->prev;
+			   temp->next->prev = temp;
+			   if (temp->prev)
+				   temp->prev->next = temp;
+			   else
+				   *list = temp;
+			   print_list(*list);
+		   }
+		current = current->next;
+	}
 }
