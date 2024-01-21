cc15c832d244ca995a2a30be0e1364371557fde3
shreen-youssif <shreenyoussif22@gmail.com>
2024-01-21 03:44:22 -0800

    2-selection_sort.c

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,5 +1,4 @@
 #include "sort.h"
-#include <stdio.h>
 
 /**
  * selection_sort - Sorts an array of integers in ascending order
@@ -10,28 +9,28 @@
  */
 void selection_sort(int *array, size_t size)
 {
-    size_t i, j, min_idx;
-    int temp;
+	size_t i, j, min_idx;
+	int temp;
 
-    if (!array || size < 2)
-        return;
+	if (!array || size < 2)
+		return;
 
-    for (i = 0; i < size - 1; i++)
-    {
-        min_idx = i;
-        for (j = i + 1; j < size; j++)
-        {
-            if (array[j] < array[min_idx])
-                min_idx = j;
-        }
+	for (i = 0; i < size - 1; i++)
+	{
+		min_idx = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min_idx])
+				min_idx = j;
+		}
 
-        if (min_idx != i)
-        {
-            /* Swap elements and print the array */
-            temp = array[i];
-            array[i] = array[min_idx];
-            array[min_idx] = temp;
-            print_array(array, size);
-        }
-    }
+		if (min_idx != i)
+		{
+			/* Swap elements and print the array */
+			temp = array[i];
+			array[i] = array[min_idx];
+			array[min_idx] = temp;
+			print_array(array, size);
+		}
+	}
 }
