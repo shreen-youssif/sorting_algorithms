386bbc60e8b2371b87ac40c822f15e6b61a583e0
shreen-youssif <shreenyoussif22@gmail.com>
2024-01-21 09:48:28 -0800

    102-counting_sort.c

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -1,54 +1,45 @@
 #include "sort.h"
-#include <stdio.h>
-#include <stdlib.h>
-
 /**
- * counting_sort - Sorts an array of integers in ascending order using the
- *                 Counting sort algorithm
+ * counting_sort - Sorts an array of integers using the Counting sort algorithm
  *
  * @array: The array to be sorted
- * @size: Number of elements in @array
+ * @size: Number of elements in the array
  */
 void counting_sort(int *array, size_t size)
 {
-    if (array == NULL || size < 2)
-        return;
+	int *count_array, *output;
+	size_t i, max = 0;
 
-    /* Find the maximum value in the array */
-    int max = array[0];
-    for (size_t i = 1; i < size; i++)
-    {
-        if (array[i] > max)
-            max = array[i];
-    }
-
-    /* Create and initialize the count array with zeros */
-    int *count_array = malloc(sizeof(int) * (max + 1));
-    if (count_array == NULL)
-        return;
-
-    for (int i = 0; i <= max; i++)
-        count_array[i] = 0;
-
-    /* Populate the count array with the occurrences of each element */
-    for (size_t i = 0; i < size; i++)
-        count_array[array[i]]++;
-
-    /* Print the count array */
-    print_array(count_array, max + 1);
-
-    /* Update the original array with sorted values using the count array */
-    size_t i = 0;
-    for (int j = 0; j <= max; j++)
-    {
-        while (count_array[j] > 0)
-        {
-            array[i] = j;
-            count_array[j]--;
-            i++;
-        }
-    }
-
-    /* Free dynamically allocated memory */
-    free(count_array);
+	if (!array || size < 2)
+		return;
+	for (i = 0; i < size; i++)
+	{
+		if ((size_t)array[i] > max)
+			max = array[i];
+	}
+	count_array = malloc(sizeof(int) * (max + 1));
+	if (!count_array)
+		return;
+	for (i = 0; i <= max; i++)
+		count_array[i] = 0;
+	for (i = 0; i < size; i++)
+		count_array[array[i]]++;
+	print_array(count_array, max + 1);
+	for (i = 1; i <= max; i++)
+		count_array[i] += count_array[i - 1];
+	output = malloc(sizeof(int) * size);
+	if (!output)
+	{
+		free(count_array);
+		return;
+	}
+	for (i = size - 1; i < size; i--)
+	{
+		output[count_array[array[i]] - 1] = array[i];
+		count_array[array[i]]--;
+	}
+	for (i = 0; i < size; i++)
+		array[i] = output[i];
+	free(count_array);
+	free(output);
 }
