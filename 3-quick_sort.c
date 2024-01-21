6bb6b6d2167c19e2bd1bebe06a56de931fbaa5ed
shreen-youssif <shreenyoussif22@gmail.com>
2024-01-21 03:51:13 -0800

    3-quick_sort.c

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -13,32 +13,32 @@
  */
 size_t lomuto_partition(int *array, int low, int high, size_t size)
 {
-    int pivot, temp;
-    int i, j;
+	int pivot, temp;
+	int i, j;
 
-    pivot = array[high];
-    i = low - 1;
+	pivot = array[high];
+	i = low - 1;
 
-    for (j = low; j <= high - 1; j++)
-    {
-        if (array[j] < pivot)
-        {
-            i++;
-            /* Swap elements and print the array */
-            temp = array[i];
-            array[i] = array[j];
-            array[j] = temp;
-            print_array(array, size);
-        }
-    }
+	for (j = low; j <= high - 1; j++)
+	{
+		if (array[j] < pivot)
+		{
+			i++;
+			/* Swap elements and print the array */
+			temp = array[i];
+			array[i] = array[j];
+			array[j] = temp;
+			print_array(array, size);
+		}
+	}
 
-    /* Swap the pivot to its correct position and print the array */
-    temp = array[i + 1];
-    array[i + 1] = array[high];
-    array[high] = temp;
-    print_array(array, size);
+	/* Swap the pivot to its correct position and print the array */
+	temp = array[i + 1];
+	array[i + 1] = array[high];
+	array[high] = temp;
+	print_array(array, size);
 
-    return (i + 1);
+	return (i + 1);
 }
 
 /**
@@ -51,15 +51,15 @@ size_t lomuto_partition(int *array, int low, int high, size_t size)
  */
 void lomuto_quick_sort(int *array, int low, int high, size_t size)
 {
-    size_t pivot;
+	size_t pivot;
 
-    if (low < high)
-    {
-        pivot = lomuto_partition(array, low, high, size);
+	if (low < high)
+	{
+		pivot = lomuto_partition(array, low, high, size);
 
-        lomuto_quick_sort(array, low, pivot - 1, size);
-        lomuto_quick_sort(array, pivot + 1, high, size);
-    }
+		lomuto_quick_sort(array, low, pivot - 1, size);
+		lomuto_quick_sort(array, pivot + 1, high, size);
+	}
 }
 
 /**
@@ -71,8 +71,8 @@ void lomuto_quick_sort(int *array, int low, int high, size_t size)
  */
 void quick_sort(int *array, size_t size)
 {
-    if (!array || size < 2)
-        return;
+	if (!array || size < 2)
+		return;
 
-    lomuto_quick_sort(array, 0, size - 1, size);
+	lomuto_quick_sort(array, 0, size - 1, size);
 }
