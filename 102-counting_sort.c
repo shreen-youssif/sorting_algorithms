4dc96fa59f9ecbb1d8c61dd8177450f33b9396ff
shreen-youssif <shreenyoussif22@gmail.com>
2024-01-22 07:30:13 -0800

    102

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -1,80 +1,49 @@
 #include "sort.h"
 
 /**
- * find_max_value - Finds the maximum value in an array of integers.
+ * counting_sort - Sorts an array of integers using Counting sort algorithm
  *
- * @arr: An array of integers.
- * @size: The size of the array.
- *
- * Return: The maximum integer in the array.
- */
-int find_max_value(int *arr, size_t size)
-{
-    int max_val, i;
-
-    for (max_val = arr[0], i = 1; i < size; i++)
-    {
-        if (arr[i] > max_val)
-            max_val = arr[i];
-    }
-
-    return (max_val);
-}
-
-/**
- * counting_sort - Sort an array of integers in ascending order
- *                 using the counting sort algorithm.
- *
- * @array: An array of integers.
- * @size: The size of the array.
- *
- * Description: Prints the counting array after setting it up.
+ * @array: The array to be sorted
+ * @size: Number of elements in @array
  */
 void counting_sort(int *array, size_t size)
 {
-    int *count_array, *sorted_array, max_val;
-    size_t i;
-
     if (array == NULL || size < 2)
         return;
 
-    sorted_array = malloc(sizeof(int) * size);
-    if (sorted_array == NULL)
-        return;
-
-    max_val = find_max_value(array, size);
-    count_array = malloc(sizeof(int) * (max_val + 1));
-    if (count_array == NULL)
+    /* Find the maximum value in the array */
+    int max = array[0];
+    for (size_t i = 1; i < size; i++)
     {
-        free(sorted_array);
-        return;
+        if (array[i] > max)
+            max = array[i];
     }
 
-    for (i = 0; i <= max_val; i++)
-        count_array[i] = 0;
+    /* Create a counting array with size (max + 1) and initialize to 0 */
+    int *count = malloc((max + 1) * sizeof(int));
+    if (count == NULL)
+        return;
 
-    for (i = 0; i < size; i++)
-        count_array[array[i]]++;
+    for (int i = 0; i <= max; i++)
+        count[i] = 0;
 
-    printf("0");
-    for (i = 1; i <= max_val; i++)
-        printf(", %d", count_array[i]);
-    printf("\n");
+    /* Count occurrences of each element in the input array */
+    for (size_t i = 0; i < size; i++)
+        count[array[i]]++;
 
-    for (i = 1; i <= max_val; i++)
-        count_array[i] += count_array[i - 1];
-
-    for (i = size - 1; i < size; i--)
+    /* Update the array with sorted values */
+    size_t j = 0;
+    for (int i = 0; i <= max; i++)
     {
-        sorted_array[count_array[array[i]] - 1] = array[i];
-        count_array[array[i]]--;
+        while (count[i] > 0)
+        {
+            array[j++] = i;
+            count[i]--;
+        }
     }
 
-    for (i = 0; i < size; i++)
-        array[i] = sorted_array[i];
+    /* Print the counting array */
+    print_array(count, max + 1);
 
-    print_array(array, size);
-
-    free(sorted_array);
-    free(count_array);
+    free(count);
 }
