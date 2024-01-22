edee63bf0ca5a71cb9d044af0f66524eeaae210e
shreen-youssif <shreenyoussif22@gmail.com>
2024-01-22 05:14:02 -0800

    102-counting_sort.c

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -1,74 +1,80 @@
 #include "sort.h"
 
 /**
- * counting_sort - Sorts an array of integers in ascending order using the
- *                 Counting sort algorithm
+ * find_max_value - Finds the maximum value in an array of integers.
  *
- * @array: The array to be sorted
- * @size: Number of elements in @array
+ * @arr: An array of integers.
+ * @size: The size of the array.
+ *
+ * Return: The maximum integer in the array.
+ */
+int find_max_value(int *arr, size_t size)
+{
+    int max_val, i;
+
+    for (max_val = arr[0], i = 1; i < size; i++)
+    {
+        if (arr[i] > max_val)
+            max_val = arr[i];
+    }
+
+    return (max_val);
+}
+
+/**
+ * counting_sort - Sort an array of integers in ascending order
+ *                 using the counting sort algorithm.
+ *
+ * @array: An array of integers.
+ * @size: The size of the array.
+ *
+ * Description: Prints the counting array after setting it up.
  */
 void counting_sort(int *array, size_t size)
 {
-    int *count_array, *output;
-    int max_value = 0;
+    int *count_array, *sorted_array, max_val;
     size_t i;
 
     if (array == NULL || size < 2)
         return;
 
-    /* Find the maximum value in the array */
-    for (i = 0; i < size; i++)
-    {
-        if (array[i] > max_value)
-            max_value = array[i];
-    }
-
-    /* Allocate memory for count_array */
-    count_array = malloc(sizeof(int) * (max_value + 1));
-    if (!count_array)
+    sorted_array = malloc(sizeof(int) * size);
+    if (sorted_array == NULL)
         return;
 
-    /* Initialize count_array to 0 */
-    for (i = 0; i <= (size_t)max_value; i++)
+    max_val = find_max_value(array, size);
+    count_array = malloc(sizeof(int) * (max_val + 1));
+    if (count_array == NULL)
+    {
+        free(sorted_array);
+        return;
+    }
+
+    for (i = 0; i <= max_val; i++)
         count_array[i] = 0;
 
-    /* Count occurrences of each element in the input array */
     for (i = 0; i < size; i++)
         count_array[array[i]]++;
 
-    /* Print count_array */
     printf("0");
-    for (i = 1; i <= (size_t)max_value; i++)
+    for (i = 1; i <= max_val; i++)
         printf(", %d", count_array[i]);
     printf("\n");
 
-    /* Update count_array to store the actual position of each element */
-    for (i = 1; i <= (size_t)max_value; i++)
+    for (i = 1; i <= max_val; i++)
         count_array[i] += count_array[i - 1];
 
-    /* Allocate memory for the output array */
-    output = malloc(sizeof(int) * size);
-    if (!output)
-    {
-        free(count_array);
-        return;
-    }
-
-    /* Build the output array */
     for (i = size - 1; i < size; i--)
     {
-        output[count_array[array[i]] - 1] = array[i];
+        sorted_array[count_array[array[i]] - 1] = array[i];
         count_array[array[i]]--;
     }
 
-    /* Copy the sorted output array back to the original array */
     for (i = 0; i < size; i++)
-        array[i] = output[i];
+        array[i] = sorted_array[i];
 
-    /* Print the sorted array */
     print_array(array, size);
 
-    /* Free allocated memory */
+    free(sorted_array);
     free(count_array);
-    free(output);
 }
